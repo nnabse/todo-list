64db4583462e1db878b67a2402d1dcd1dffcaeb9
nabse <nnabse@gmail.com>
2022-07-14 15:27:45 +0300

    backend support, edit form fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const allTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+let allTasks = [];
 
 let input = "";
 let inputValue = "";
@@ -8,68 +8,119 @@ let renameInput = "";
 
 let editVisible = [0, false];
 
-window.onload = init = () => {
+window.onload = init = async () => {
   input = document.getElementById("text_input");
   input.addEventListener("change", updateValue);
+  const resp = await fetch("http://localhost:8000/allTasks", {
+    method: "GET",
+  });
+  const result = await resp.json();
+  allTasks = result.data;
+
   render();
 };
 
-const addTask = () => {
+const addTask = async () => {
   if (inputValue !== "" && inputValue !== " ") {
-    allTasks.push({
-      text: inputValue,
-      isChecked: false,
-      isEdit: false,
+    const resp = await fetch("http://localhost:8000/createTask", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        text: inputValue,
+        isCheck: false,
+        isEdit: false
+      }),
     });
+    const result = await resp.json();
+    allTasks = result.data;
   }
 
-  localStorage.setItem("tasks", JSON.stringify(allTasks));
   inputValue = "";
   input.value = "";
+
   render();
 };
 
-const onCheckboxChange = (index) => {
-  allTasks[index].isChecked = !allTasks[index].isChecked;
-  localStorage.setItem("tasks", JSON.stringify(allTasks));
+const onCheckboxChange = async (index) => {
+  const resp = await fetch(`http://localhost:8000/updateTask?id=${allTasks[index].id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        id: allTasks[index].id,
+        isCheck: !allTasks[index].isCheck
+      }),
+  });
+  const result = await resp.json();
+  allTasks = result.data;
   render();
 };
 
-const deleteTask = (index) => {
-  allTasks.splice(index, 1);
-  localStorage.setItem("tasks", JSON.stringify(allTasks));
-  editVisible = false;
+const deleteTask = async (index) => {
+  const resp = await fetch(`http://localhost:8000/deleteTask/?id=${allTasks[index].id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "Access-Control-Allow-Origin": "*"
+      },
+    });
+  const result = await resp.json();
+  allTasks = result.data;
   render();
 };
+
+let countOpened = 0;
 
 const toggleEditVisible = (index) => {
-  allTasks[index].isEdit = !allTasks[index].isEdit;
-  render();
+  allTasks.forEach((elem) => {
+    if (countOpened === 0) {
+      allTasks[index].isEdit = true;
+      renameValue = renameInput.value
+      countOpened++;
+      render();
+    }
+    if (elem.isEdit) {
+      return 0;
+    }
+  });
 };
 
 const updateValue = (event) => {
   inputValue = event.target.value;
-  localStorage.setItem("tasks", JSON.stringify(allTasks));
 };
 
-const updateRenameValue = (event) => {
-  renameValue = event.target.value;
-  localStorage.setItem("tasks", JSON.stringify(allTasks));
-};
-
-const onRename = (index) => {
+const onRename = async (index, id) => {
   if (renameValue !== "" && renameValue !== " ") {
-    allTasks[index].text = renameValue;
+    const resp = await fetch(`http://localhost:8000/updateTask?id=${allTasks[index].id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify({
+          id: allTasks[index].id,
+          text: renameValue,
+          isCheck: allTasks[index].isCheck
+        }),
+    });
+    const result = await resp.json();
+    allTasks = result.data;
     allTasks[index].isEdit = !allTasks[index].isEdit;
   }
-  localStorage.setItem("tasks", JSON.stringify(allTasks));
+  countOpened = 0;
+  allTasks[index].isEdit = false;
   render();
 };
 
 const onCancel = (index) => {
+  countOpened = 0;
   allTasks[index].text = allTasks[index].text;
   allTasks[index].isEdit = !allTasks[index].isEdit;
-  localStorage.setItem("tasks", JSON.stringify(allTasks));
   render();
 };
 
@@ -78,10 +129,10 @@ const render = () => {
   while (content.firstChild) {
     content.removeChild(content.firstChild);
   }
-  allTasks.sort((a, b) =>
-    a.isChecked > b.isChecked ? 1 : a.isChecked < b.isChecked ? -1 : 0
-  );
+  allTasks.sort((a, b) => a.isCheck > b.isCheck ? 1 : a.isCheck < b.isCheck ? -1 : 0);
   allTasks.map((item, index) => {
+    const itemId = allTasks[index].id;
+
     const container = document.createElement("div");
     container.id = `task_${index}`;
     container.className = "task";
@@ -89,17 +140,22 @@ const render = () => {
     const editForm = document.createElement("input");
     editForm.className = "rename_input";
     editForm.value = allTasks[index].text;
+    editForm.id = itemId;
+
+    const updateRenameValue = (event) => {
+      renameValue = event.target.value;
+    };
 
     editForm.addEventListener("change", updateRenameValue);
 
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    checkbox.checked = item.isChecked;
+    checkbox.checked = item.isCheck;
     checkbox.onchange = () => onCheckboxChange(index);
 
     const text = document.createElement("p");
     text.innerText = item.text;
-    text.className = item.isChecked ? "task-text done-text" : "task-text";
+    text.className = item.isCheck ? "task-text done-text" : "task-text";
 
     const editButton = document.createElement("img");
     editButton.src = "images/edit.svg";
@@ -111,7 +167,7 @@ const render = () => {
 
     const renameButton = document.createElement("img");
     renameButton.src = "images/done.svg";
-    renameButton.onclick = () => onRename(index);
+    renameButton.onclick = () => onRename(index, item.id);
 
     const cancelRenameButton = document.createElement("img");
     cancelRenameButton.src = "images/cancel.svg";
